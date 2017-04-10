5253165b6aa40a653d7c4a7e676b0961462046c7
Aaron Nguyen and Ilene Gorski <aaron+ilene@>
2017-04-10 15:26:41 -0700

    Add info list on click

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,8 @@ function Pet(name, animal, colors, age, breed) {
   this.colors = colors;
   this.age = age;
   this.breed = breed;
+  this.adopted = false;
 }
-
-
 
 Pet.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
@@ -33,78 +32,38 @@ $(document).ready(function() {
   var petColor = $("input#pet-color").val();
   var petBreed = $("input#pet-breed").val();
 
-  var newPet = new Pet(petName, petType, petAge, petColor, petBreed);
+  var newPet = new Pet(petName, petType, petColor, petAge, petBreed);
   console.log(newPet);
   newPet.petInfo();
-
-  $(".new-pet").append(
-      "<li>" + newPet.name + "</li>" +
-      "<li>" + newPet.animal + "</li>" +
-      "<li>" + newPet.colors + "</li>" +
-      "<li>" + newPet.age + "</li>" +
-      "<li>" + newPet.breed + "</li>" +
-      "<li>" + newPet.adopted + "</li>"
-      //add adopt button when adopted === false
-);
-
-    // "<li>"+ newPet.petInfo() + "</li>");
-
-
-
-  $(".test").click(function(event) {
-    event.preventDefault();
-    newPet.petInfo();
-    console.log(newPet.petInfo());
-
-  });
-  $(".added-pet").click(function() {
+  $(".added-pet").append(
+      "<div class='panel'>" +
+      "<div class='panel panel-heading'>" + newPet.name + "</div>" +
+      "<div class='panel-body'>" +
+          // "<ul>" +
+          //   "<li>" + newPet.name + "</li>" +
+          //   "<li>" + newPet.animal + "</li>" +
+          //   "<li>" + newPet.colors + "</li>" +
+          //   "<li>" + newPet.age + "</li>" +
+          //   "<li>" + newPet.breed + "</li>" +
+          //   "<li>" + newPet.adopted + "</li>" +
+          //   //add adopt button when adopted === false
+          // "</ul>" +
+        "</div>" +
+      "</div>");
 
 
-
-  });
-
-
-    //   "<table>" +
-    //  "<tr>" +
-    //    "<th>image</th>" +
-    //    "<th>name</th>" +
-    //    "<th>animal</th>" +
-    //    "<th>colors</th>" +
-    //    "<th>age</th>" +
-    //    "<th>breed</th>" +
-    //    "<th>adopted</th>" +
-    //  "</tr>" +
-    //  "<tr>" +
-    //   "<td>"+newPet.image+"</td>" +
-    //   "<td>"+newPet.name+"</td>" +
-    //   "<td>"+newPet.animal+"</td>" +
-    //   "<td>"+newPet.colors+"</td>" +
-    //   "<td>"+newPet.age+"</td>" +
-    //   "<td>"+newPet.breed+"</td>" +
-    //   "<td>"+newPet.adopted+"</td>" +
-    //  "</tr>" +
-    // "</table>"
-
-  });//end add pet
+     $(".added-pet").last().click(function() {
+       $(".panel-body").show();
+       $(".panel-body").append("<ul>" +
+         "<li>" + newPet.name + "</li>" +
+         "<li>" + newPet.animal + "</li>" +
+         "<li>" + newPet.colors + "</li>" +
+         "<li>" + newPet.age + "</li>" +
+         "<li>" + newPet.breed + "</li>" +
+         "<li>" + newPet.adopted + "</li>" +
+         //add adopt button when adopted === false
+       "</ul>");
+       console.log(newPet.colors);
+     });
+   });
 });
-
-
-
-
-// backend
-
-//
-// function Image() {
-//
-// }
-var img = new Image();   // Create new img element
-userImage = $("#image").val();
-img.src = userImage;
-
-// ui
-$(function() {
-  $(".clicky").click(function(event) {
-    event.preventDefault();
-    $("body").addClass("blue");
-  })
-})
