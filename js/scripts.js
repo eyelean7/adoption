9d4071b2d07fdb4be0a056a5dd55e0b2a58d4028
Aaron Nguyen and Ilene Gorski <aaron+ilene@>
2017-04-10 13:48:38 -0700

    Add buttons

--- js/scripts.js
+++ js/scripts.js
@@ -16,28 +16,20 @@ Pet.prototype.fullName = function() {
 }
 
 Pet.prototype.petInfo = function() {
-  // return
-// "<table>" +
-//  "<tr>" +
-//    "<th>image</th>" +
-//    "<th>name</th>" +
-//    "<th>animal</th>" +
-//    "<th>colors</th>" +
-//    "<th>age</th>" +
-//    "<th>breed</th>" +
-//    "<th>adopted</th>" +
-//  "</tr>" +
-//  "<tr>" +
-//   "<td>"this.image"</td>" +
-//   "<td>"this.name"</td>" +
-//   "<td>"this.animal"</td>" +
-//   "<td>"this.colors"</td>" +
-//   "<td>"this.age"</td>" +
-//   "<td>"this.breed"</td>" +
-//   "<td>"this.adopted"</td>" +
-//  "</tr>" +
-// "</table>"
-console.log(this.name, this.animal, this.age);
+  return "hello"
+// "<div>" +
+//  "<ul>" +
+//   "<li>" + this.image + "</li>" +
+//   "<li>" + this.name + "</li>" +
+//   "<li>" + this.animal + "</li>" +
+//   "<li>" + this.colors + "</li>" +
+//   "<li>" + this.age + "</li>" +
+//   "<li>" + this.breed + "</li>" +
+//   "<li>" + this.adopted + "</li>" +
+//  "</ul>" +
+// "</div>";
+// console.log(this.name, this.animal, this.age);
+
 }
 
 
@@ -49,78 +41,52 @@ $(document).ready(function() {
     $(".pet").show();
     $(".pet").submit(function(event) {
     event.preventDefault();
-
-
     var petName = $("input#pet-name").val();
     var petType = $(".size").val();
     var petAge = $("input#pet-age").val();
     var newPet = new Pet("image", petName, petType, ["blue", "greeen"], petAge, "calico", false);
     console.log(newPet);
+    $(".new-pet").append(
+      "<li>"+ newPet.petInfo() + "</li>");
+
+
+
     $(".test").click(function(event) {
       event.preventDefault();
       newPet.petInfo();
+      console.log(newPet.petInfo());
+
     });
+    $(".added-pet").click(function() {
+
+
     });
   });
 
-  $(".add-pet").click(function() {
-    $(".new-pet").append("<table>" +
-     "<tr>" +
-       "<th>image</th>" +
-       "<th>name</th>" +
-       "<th>animal</th>" +
-       "<th>colors</th>" +
-       "<th>age</th>" +
-       "<th>breed</th>" +
-       "<th>adopted</th>" +
-     "</tr>" +
-     "<tr>" +
-      "<td>"+image+"</td>" +
-      "<td>"+name+"</td>" +
-      "<td>"+animal+"</td>" +
-      "<td>"+colors+"</td>" +
-      "<td>"+age+"</td>" +
-      "<td>"+breed+"</td>" +
-      "<td>"+adopted+"</td>" +
-     "</tr>" +
-    "</table>");
+
+    //   "<table>" +
+    //  "<tr>" +
+    //    "<th>image</th>" +
+    //    "<th>name</th>" +
+    //    "<th>animal</th>" +
+    //    "<th>colors</th>" +
+    //    "<th>age</th>" +
+    //    "<th>breed</th>" +
+    //    "<th>adopted</th>" +
+    //  "</tr>" +
+    //  "<tr>" +
+    //   "<td>"+newPet.image+"</td>" +
+    //   "<td>"+newPet.name+"</td>" +
+    //   "<td>"+newPet.animal+"</td>" +
+    //   "<td>"+newPet.colors+"</td>" +
+    //   "<td>"+newPet.age+"</td>" +
+    //   "<td>"+newPet.breed+"</td>" +
+    //   "<td>"+newPet.adopted+"</td>" +
+    //  "</tr>" +
+    // "</table>"
+
   });//end add pet
 });
-
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
-
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
-
-    $(".new-address").each(function() {
-      var inputtedType = $(this).find("select.new-type").val();
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-
-      console.log(inputtedType, inputtedStreet,inputtedCity, inputtedState);
-      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState, inputtedType);
-      newContact.addresses.push(newAddress);
-    });
-
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
-
-    resetFields();
-
-  });
 
 
 
